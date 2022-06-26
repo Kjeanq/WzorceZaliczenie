27c8d0ba1a95f9ed1ac0cbc3e4bb19d12a6aeabb
Kjeanq <63155345+Kjeanq@users.noreply.github.com>
2022-06-26 19:25:08 +0200

    Patterns added Composite Builder Singleton

--- src/main/java/zaliczenie/projekt/wzorceProjektowe/controllers/LoginController.java
+++ src/main/java/zaliczenie/projekt/wzorceProjektowe/controllers/LoginController.java
@@ -2,16 +2,25 @@ package zaliczenie.projekt.wzorceProjektowe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import zaliczenie.projekt.wzorceProjektowe.backend.LoginBackend;
+import zaliczenie.projekt.wzorceProjektowe.database.tables.User;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/")
-    public String getLogin(Model model) {
-        model.addAttribute("logowanie", "aaaa");
-
+    public String getLogin( Model model) {
         return "login";
+    }
+    @RequestMapping(value = "/logowanie", method = RequestMethod.POST)
+    public ModelAndView logging(@ModelAttribute User user) {
+
+        return new ModelAndView();
     }
 
 }
